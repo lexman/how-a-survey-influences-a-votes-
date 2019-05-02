31593411e61edf47212ebf721323b20e3869e3f8
lexman <android@lexman.org>
2019-05-02 23:20:29 +0200

    Nicer plots

--- mk_responses/mkplots.py
+++ mk_responses/mkplots.py
@@ -2,14 +2,26 @@
 
 import numpy as np
 import matplotlib.pyplot as plt
-height = [53, 47]
-bars = ('Oui', 'Non')
-y_pos = np.arange(len(bars))
-plt.barh(bars, height, color=(0.2, 0.4, 0.6, 0.6))
- 
-# Custom Axis title
-plt.xlabel(u'Réponses (%)', fontweight='bold', color = 'grey', fontsize='17', horizontalalignment='center')
-plt.yticks(y_pos, bars, color='grey', fontweight='bold', fontsize='17', horizontalalignment='right')
- 
 
-plt.show()
+
+def mk_response(pct_yes):
+    height = [pct_yes, 100 - pct_yes]
+    bars = ('Oui', 'Non')
+    y_pos = np.arange(len(bars))
+    plt.figure(figsize=(5.6,2.5))
+    plt.barh(bars, height, color=(0.2, 0.4, 0.6, 0.6))
+    plt.xlabel(u'Réponses (%)', fontweight='bold', color = 'grey', fontsize='17', horizontalalignment='center')
+    plt.yticks(y_pos, bars, color='grey', fontweight='bold', fontsize='17', horizontalalignment='right')
+    label_yes = "{} %".format(pct_yes)
+    plt.text(40, -0.1, label_yes, fontsize=17, fontweight='bold', color='#404040')
+    label_no = "{} %".format(100 - pct_yes)
+    plt.text(40, .9, label_no, fontsize=17, fontweight='bold', color='#404040')
+     
+    plt.savefig("../opendata/imgs/.private/responses/response_{}.png".format(pct_yes))
+    plt.clf()
+    #plt.show()
+
+for i in range(1, 100):
+    mk_response(i)
+
+#mk_response(53)
